5336fde06838fa3ab5be115c8e0252ce2d7b6e19
Jason Lee <lwlee2608@gmail.com>
2023-08-04 10:48:41 +0800

    some clean up

--- src/docker_compose.rs
+++ src/docker_compose.rs
@@ -26,7 +26,7 @@ impl DockerCompose {
             .expect("Failed to execute command");
         println!("Output: {}", String::from_utf8_lossy(&output.stdout));
         println!("Errors: {}", String::from_utf8_lossy(&output.stderr));
-        println!("Starting Test");
+        println!("Docker Compose started");
 
         let dir = &self.logs_dir;
         if Path::new(dir).exists() {
@@ -51,33 +51,35 @@ impl DockerCompose {
                 let file_name = format!("{}/{}.log", dir, container);
                 let file_path = std::path::PathBuf::from(file_name);
                 let docker_compose_file = self.file.clone();
-                thread::spawn(move || DockerCompose::follow_container_log(docker_compose_file, container, file_path))
+                thread::spawn(move || {
+                    let follow_container_log = |container: String, file_path: std::path::PathBuf| {
+                        let file = File::create(file_path).unwrap();
+                        let _ = Command::new("docker-compose")
+                            .arg("-f")
+                            .arg(docker_compose_file)
+                            .arg("logs")
+                            .arg("--follow")
+                            .arg("--no-log-prefix")
+                            .arg(&container)
+                            .stdout(Stdio::from(file))
+                            .spawn()
+                            .unwrap();
+                    };
+                
+                    follow_container_log(container, file_path);
+                });
             })
             .collect();
     }
 
     pub fn down(&self) {
+        println!("Gracefully shutting down...");
+
         let _output = Command::new("docker-compose")
             .arg("-f")
             .arg(self.file.clone())
             .arg("down")
             .output()
             .expect("Failed to execute command");
-
-        println!("Gracefully shutting down...");
-    }
-
-    fn follow_container_log(docker_compose_file: String, container: String, file_path: std::path::PathBuf) {
-        let file = File::create(file_path).unwrap();
-        let _ = Command::new("docker-compose")
-            .arg("-f")
-            .arg(docker_compose_file)
-            .arg("logs")
-            .arg("--follow")
-            .arg("--no-log-prefix")
-            .arg(&container)
-            .stdout(Stdio::from(file))
-            .spawn()
-            .unwrap();
     }
 }
